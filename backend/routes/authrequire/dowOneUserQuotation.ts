6c629f1ff43f5b02057f3514ad1747246795ad5c
Iicytower <szymonfirkowicz@gmail.com>
2021-01-28 15:29:52 +0100

    add doeOneQuotation endpoint

---
+++ backend/routes/authrequire/dowOneUserQuotation.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import * as bodyParser from 'body-parser';
+import validator from "../../middlewares/validator";
+import { check } from "express-validator";
+
+const router = Router();
+
+import dowOneUserQuotation from '../../controllers/authrequire/dowOneUserQuotation';
+
+router.get('/', 
+bodyParser.json(),
+[
+   check("name").isString(),
+],
+validator(),
+dowOneUserQuotation);
+
+export default router;
