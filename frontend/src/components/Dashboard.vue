fc558d66aae8f6fd6401995d514abc97078e0e3b
Iicytower <szymonfirkowicz@gmail.com>
2021-03-24 12:31:14 +0100

    quotations list done

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -5,6 +5,21 @@
       <h1>This is a dashboard page</h1>
       <a href="http://localhost:8080/">Home Page Link</a>
 
+      <ul>
+        <li  class="listItem" v-for="el in quotationsList.data" v-bind:key="el._id">
+
+          <div class="name"><h3>{{ el.name }}</h3></div>
+          <div class="dates">
+            <div class="dateItem">last update: <br> {{ getYMD(el.createdAt) }} </div>
+            <div class="dateItem">created at: <br> {{ getYMD(el.updatedAt) }} </div>
+          </div>
+          <div class="delEditBtns">
+            <button type="button" class="delete" v-bind:id="el._id">delete</button>
+          </div>  
+          
+        </li>
+      </ul>
+
     </div>
   </div>
 </template>
@@ -14,7 +29,7 @@
 export default {
   data() {
     return {
-
+      quotationsList: {},
     }
   },
   async created(){
@@ -23,13 +38,20 @@ export default {
   methods: {
     async currentUserQuotationsList(){
       try {
-
+        const quotationsList = await fetchQuotationsList()
         console.log('dowAllUserQuotations');
-        console.log('dowAllUserQuotations', await fetchQuotationsList());
+        console.log('dowAllUserQuotations', quotationsList);
+        this.quotationsList = quotationsList;
+
+
       } catch (err) {
         console.error(error);
         alert("Something goes wrong. Please try later. dash")
       }
+    },
+    getYMD(date){
+      const d = new Date(date);
+      return `${d.getDate()}.${d.getMonth() + 1}.${d.getFullYear()}`
     },
   },
 }
